611d5fe60ebe49961c958aa970eed14797abcf2f
joisgori <00034715@uca.edu.sv>
2019-06-03 01:07:42 -0600

    Ya no sé qué pasó, no me reconoce los eventos de onClickListener :c, falta adapter t.t

--- app/src/main/java/com/example/registrobkb/MainActivity.kt
+++ app/src/main/java/com/example/registrobkb/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.registrobkb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //Doy funcionalidad de listener a mi text
+    val tvBtn1 = findViewById(R.id.tv_nuevopartido) as TextView
+    val tvBtn2 = findViewById<TextView>(R.id.tv_partidosguardados)
+    val dfs = findViewById<Button>(R.id.id_butttooon)
+
+
+
 }
